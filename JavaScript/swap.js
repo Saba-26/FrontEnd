81060916496008ffff98063f210e00f18ee6ad83
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-03 01:46:55 +0530

    swapping using xor

--- JavaScript/swap.js
+++ JavaScript/swap.js
@@ -1,8 +1,19 @@
 //swapping 2 variables using temp
+// var a=10;
+// var b=20;
+// a=a+b;
+// b=a-b;
+// a=a-b;
+// console.log("a = "+a);
+// console.log("b = "+b);
+
+//using bit wise operator
 var a=10;
 var b=20;
-a=a+b;
-b=a-b;
-a=a-b;
-console.log("a = "+a);
-console.log("b = "+b);
+a=a^b;
+// console.log(a);
+b=a^b;
+
+a=a^b;
+console.log(a);
+console.log(b);
